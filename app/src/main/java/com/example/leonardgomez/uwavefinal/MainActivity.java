698c10527eb1b345be102dc7688a08add80e8c90
Leonard Gomez <leonardgomez93@gmail.com>
2018-03-07 13:20:53 -0800

    new branches

--- app/src/main/java/com/example/leonardgomez/uwavefinal/MainActivity.java
+++ app/src/main/java/com/example/leonardgomez/uwavefinal/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // Handle navigation view item clicks heres.
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
