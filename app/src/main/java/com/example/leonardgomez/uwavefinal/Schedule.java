87683fe08038c61bae7f7974fb64334000f5dfc1
Etienne Sotelo <ems43@uw.edu>
2018-03-09 20:01:08 -0800

    Newest update

--- app/src/main/java/com/example/leonardgomez/uwavefinal/Schedule.java
+++ app/src/main/java/com/example/leonardgomez/uwavefinal/Schedule.java
@@ -6,10 +6,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 
 
 
 public class Schedule extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+        WebView webview = (WebView)findViewById(R.id.webview);
+
+
+
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.loadUrl("https://calendar.google.com/calendar/embed?src=vfgkklo6vnqh4j7iu4b9ffqgvs%40group.calendar.google.com&ctz=America%2FLos_Angeles");
+
+    }
 
 
 
