047b8a8f42c31a321c470636a717b0363c6fbcd3
tweatherbee <35078278+tweatherbee@users.noreply.github.com>
2018-03-10 20:39:40 -0800

    added new menu icons, made menu items larger, added comment about Calendar implementation

--- app/src/main/java/com/example/leonardgomez/uwavefinal/Schedule.java
+++ app/src/main/java/com/example/leonardgomez/uwavefinal/Schedule.java
@@ -26,9 +26,23 @@ public class Schedule extends MainActivity implements NavigationView.OnNavigatio
 
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        // Pulls web page of UWave's calendar and displays within the WebView
         webview.loadUrl("https://calendar.google.com/calendar/embed?src=vfgkklo6vnqh4j7iu4b9ffqgvs%40group.calendar.google.com&ctz=America%2FLos_Angeles");
         webview.setVerticalScrollBarEnabled(true);
         webview.setHorizontalScrollBarEnabled(true);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
