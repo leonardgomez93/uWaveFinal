7d47e67c945c7f12da087d4d8babd224d46c5aa9
Chris <oungc1@github.com>
2018-03-10 16:54:36 -0800

    Google form place holder for the MyWave page.

--- app/src/main/java/com/example/leonardgomez/uwavefinal/MyWave.java
+++ app/src/main/java/com/example/leonardgomez/uwavefinal/MyWave.java
@@ -1,15 +1,38 @@
 package com.example.leonardgomez.uwavefinal;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
 
 
 public class MyWave extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_wave);
+        WebView myWave = (WebView) findViewById(R.id.webview);
 
+        WebSettings webSettings = myWave.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWave.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSfh8DdB22l7kjb_akwjjs8pPaW6XcwPccCK3FcRmcGrkVWBsA/viewform");
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
 
 }
