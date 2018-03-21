ab1df50b91400c48a5d42cedd49d0ba00150ab2f
Chris Oung <oungc1@uw.edu>
2018-03-21 11:28:17 -0700

    Google Calendar working!

--- app/src/main/java/com/example/leonardgomez/uwavefinal/Community.java
+++ app/src/main/java/com/example/leonardgomez/uwavefinal/Community.java
@@ -6,10 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.app.Application;
 
 
-public class Community extends MainActivity {
+
+public class  Community extends MainActivity {
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.content_community);
 
 
+    }
 
 }
