b0fb02806f9f35ff29ce83da97eb26f59348541b
leonardgomez93 <leonardgomez93@gmail.com>
2018-03-22 01:57:34 -0700

    calendar success

--- app/src/main/java/com/example/leonardgomez/uwavefinal/fetchData.java
+++ app/src/main/java/com/example/leonardgomez/uwavefinal/fetchData.java
@@ -20,7 +20,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
