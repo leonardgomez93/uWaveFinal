3ea1817587d5afc1757b502d81476e9dc5d6b788
Chris Oung <oungc1@uw.edu>
2018-03-31 09:19:22 -0700

    Chat is working. Sign-in via firebase and follow the authentication prompts.

---
+++ app/src/main/java/com/example/leonardgomez/uwavefinal/uwavechat/ChatBubble.java
@@ -0,0 +1,26 @@
+package com.example.leonardgomez.uwavefinal.uwavechat;
+
+/**
+ * Created by chrisoung on 3/30/18.
+ */
+
+public class ChatBubble {
+
+    private String content;
+    private boolean myMessage;
+
+    public ChatBubble(String content, boolean myMessage) {
+        this.content = content;
+        this.myMessage = myMessage;
+    }
+
+    public String getContent() {
+        String content = this.content;
+        return content;
+    }
+
+    public boolean myMessage() {
+        boolean myMessage = this.myMessage;
+        return myMessage;
+    }
+}
---
+++ app/src/main/java/com/example/leonardgomez/uwavefinal/uwavechat/UserDetails.java
@@ -0,0 +1,11 @@
+package com.example.leonardgomez.uwavefinal.uwavechat;
+
+/**
+ * Created by chrisoung on 3/30/18.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
