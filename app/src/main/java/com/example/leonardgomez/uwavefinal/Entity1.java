27096024c2417112cb09efa9d1886aaaa25ce66b
leonardgomez93 <leonardgomez93@gmail.com>
2018-04-18 09:34:04 -0700

    calendar successssaa

---
+++ app/src/main/java/com/example/leonardgomez/uwavefinal/Entity1.java
@@ -0,0 +1,23 @@
+package com.example.leonardgomez.uwavefinal;
+
+/**
+ * Created by leonardgomez on 4/6/18.
+ */
+
+public class Entity1 {
+
+    public String Name;
+    public String isOne;
+    public String refID;
+    public int isFrist;
+    public String jobID;
+
+    // *Week View
+    public String JobIDForButton;
+    public String JobRefID;
+
+    // *remove layout
+    public int layoutView;
+    public int buttonTag;
+
+}
